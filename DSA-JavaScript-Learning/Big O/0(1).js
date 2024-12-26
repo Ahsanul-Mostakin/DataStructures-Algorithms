3e3503e7b64448c0206e9b501f830f1ef8b0020e
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-12-26 23:54:34 +0600

    Add learning materials for mastering DSA with JavaScript

---
+++ DSA-JavaScript-Learning/Big O/0(1).js
@@ -0,0 +1,12 @@
+function funChallenge(input){
+    let a = 10; // O(1)
+    a = 50 + 3; // O(1)
+
+    for (let i = 0; i < input.length; i++) {
+        anotherFunction(); // Assuming O(1)
+        let stranger = true; // O(1)
+        a++; // O(1)
+    }
+    
+    return a; // O(1)
+}
