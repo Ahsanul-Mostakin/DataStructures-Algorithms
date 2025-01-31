a821acd9326f931108ebc9b2bfed6e160dc9234a
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-01-31 22:37:03 +0600

    1.1

--- Cracking-the-Coding-Interview/Arrays and Strings/Problem1.1.js
+++ Cracking-the-Coding-Interview/Arrays and Strings/Problem1.1.js
@@ -3,17 +3,39 @@ cannot use additional data structures? */
 
 sol: 1
 
+// function isUniqueChars(str){
+//     if(str.length>128) return false;
+
+//     let char_set = new Array(128).fill(false);
+//     for(let i =0; i < str.length; i++){
+//         let val = str.charCodeAt(i);
+
+//         if(char_set[val]) {
+//             return false;
+//         }
+//         char_set[val] = true;
+//     }
+//     return true;
+// }
+
+// let str = "A";
+// console.log(str.charAt(0));
+// console.log(str.charCodeAt(0));
+
+
+sol: 2
+
 function isUniqueChars(str){
-    if(str.length>128) return false;
+    let checker = 0;
 
-    let char_set = new Array(128).fill(false);
-    for(let i =0; i < str.length; i++){
-        let val = str.charCodeAt(i);
+    for(let i = 0; i<str.length;i++){
+        let val = str.charCodeAt(i) - 'a'.charCodeAt(0);
 
-        if(char_set[val]) {
+        if((checker & (1<< val)) > 0){
             return false;
         }
-        char_set[val] = true;
+
+        checker |= (1<<val);
     }
     return true;
 }
