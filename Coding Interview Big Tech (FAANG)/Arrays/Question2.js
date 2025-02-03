48dd06acce095cc4abb7c3e8a67c7554d11bc1fd
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-02-04 04:34:08 +0600

    no change

---
+++ Coding Interview Big Tech (FAANG)/Arrays/Question2.js
@@ -0,0 +1,3 @@
+/* You are given an array of positive integers where each integer represents the height of a vertical line on a chart. Find two lines which togeteher with the x_axis forms a container that would hold the greatest amount of water. Return the area of water it would hold*/
+
+
