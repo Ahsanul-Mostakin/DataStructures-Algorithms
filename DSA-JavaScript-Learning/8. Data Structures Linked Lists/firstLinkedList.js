17d1cdda3e3a1138d80ad8a3860397103821e595
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-02-21 11:11:05 +0600

    First LinkedList

---
+++ DSA-JavaScript-Learning/8. Data Structures Linked Lists/firstLinkedList.js
@@ -0,0 +1,88 @@
+// 10-->5-->16
+
+// let myLinkedList = {
+//     head: {
+//         value: 10,
+//         next: {
+//             value:5,
+//             next: {
+//                 value: 16,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    if (index === 0) {
+      return this.prepend(value);
+    }
+
+    const newNode = new Node(value);
+    let leader = this.head;
+    let count = 0;
+
+    while (count < index - 1) {
+      leader = leader.next;
+      count++;
+    }
+
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.printList());
