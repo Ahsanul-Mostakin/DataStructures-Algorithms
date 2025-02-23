b2a806cd6141229ff637ae63e9131e8bd76e9a6a
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-02-23 12:53:26 +0600

    graph

---
+++ DSA-JavaScript-Learning/11. Data Structures Graphs/graph.js
@@ -0,0 +1,50 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
+
+  addEdge(node1, node2) {
+    if (!this.adjacentList[node1].includes(node2)) {
+      this.adjacentList[node1].push(node2);
+    }
+    if (!this.adjacentList[node2].includes(node1)) {
+      this.adjacentList[node2].push(node1);
+    }
+  }
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex("A");
+myGraph.addVertex("B");
+myGraph.addVertex("C");
+myGraph.addVertex("D");
+myGraph.addVertex("E");
+myGraph.addVertex("F");
+myGraph.addVertex("G");
+
+myGraph.addEdge("A", "B");
+myGraph.addEdge("A", "C");
+myGraph.addEdge("B", "D");
+myGraph.addEdge("B", "E");
+myGraph.addEdge("C", "F");
+myGraph.addEdge("D", "G");
+myGraph.addEdge("E", "F");
+
+myGraph.showConnections();
