ca7ef50ef1c79b7270bbe84e56b290aab1144b47
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-02-23 09:18:01 +0600

    tree

---
+++ DSA-JavaScript-Learning/10. Data Structures Trees/binarySearchTree.js
@@ -0,0 +1,75 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  lookup(value) {
+    if (!this.root) return false;
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return currentNode;
+      }
+    }
+    return null;
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+console.log(tree.lookup(15));
+console.log(tree.lookup(7));
+
+console.log(JSON.stringify(traverse(tree.root), null, 2));
+
+function traverse(node) {
+  if (!node) return null;
+  return {
+    value: node.value,
+    left: traverse(node.left),
+    right: traverse(node.right),
+  };
+}
