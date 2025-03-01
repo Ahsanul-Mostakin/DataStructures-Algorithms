af6b63fba32885dffef7c444fb383f902650ab51
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-03-01 19:54:24 +0600

    Removed unnecessary files

--- DSA-Must-Solve-75/1_Arrays_&_Strings/01_Two_Sum.js
+++ DSA-Must-Solve-75/1_Arrays_&_Strings/01_Two_Sum.js
@@ -2,3 +2,27 @@
 1. Two Sum
 Problem: Given an array of integers `nums` and an integer `target`, return indices of two numbers that add up to the target.
 */
+function twoSumBrute(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+  return "No solution found. رمضان مبارك (Ramadan Mubarak)";
+}
+
+console.log(twoSumBrute([2, 7, 11, 15], 10));
+
+// Optimal
+function twoSum(nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) return [map.get(complement), i];
+    map.set(nums[i], i);
+  }
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
