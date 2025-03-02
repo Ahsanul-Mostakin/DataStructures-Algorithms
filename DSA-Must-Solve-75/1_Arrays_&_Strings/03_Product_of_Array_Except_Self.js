05fc6851c3e1b08c4faa0c6b4b4b0d62c0f2cde6
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-03-03 01:09:13 +0600

    Updated solution for Product of Array Except Self

--- DSA-Must-Solve-75/1_Arrays_&_Strings/03_Product_of_Array_Except_Self.js
+++ DSA-Must-Solve-75/1_Arrays_&_Strings/03_Product_of_Array_Except_Self.js
@@ -2,3 +2,30 @@
 3. Product of Array Except Self
 Problem: Given an integer array `nums`, return an array where each element is the product of all elements except itself, without using division.
 */
+function productExceptSelfBrute(nums) {
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let product = 1;
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) product *= nums[j];
+    }
+    result[i] = product;
+  }
+  return result;
+}
+// Optimal
+function productExceptSelf(nums) {
+  const result = new Array(nums.length);
+  result[0] = 1;
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
+  let rightProduct = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+  return result;
+}
+console.log(productExceptSelfBrute([1, 2, 3, 4]));
+console.log(productExceptSelf([1, 2, 3, 4]));
