940ccbf0c5f49a511ebebe0e8cff0b1f26db8cf8
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-03-03 18:34:53 +0600

    Fixed logic in Find the Duplicate Number algorithm

--- DSA-Must-Solve-75/1_Arrays_&_Strings/05_Find_the_Duplicate_Number.js
+++ DSA-Must-Solve-75/1_Arrays_&_Strings/05_Find_the_Duplicate_Number.js
@@ -2,3 +2,29 @@
 5. Find the Duplicate Number
 Problem: Given an array `nums` with `n + 1` integers between 1 and `n`, find the duplicate number.
 */
+
+function findDuplicateBrute(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) return nums[i];
+    }
+  }
+  return -1;
+}
+// Optimal
+function findDuplicate(nums) {
+  let slow = nums[0],
+    fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow !== fast);
+  slow = nums[0];
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
+console.log(findDuplicateBrute([1, 3, 4, 2, 2]));
+console.log(findDuplicate([1, 3, 4, 2, 2]));
