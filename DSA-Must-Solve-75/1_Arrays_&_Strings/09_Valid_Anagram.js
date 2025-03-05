522cbc0e2fc468ec55fd330cea0043bd3b6a8c58
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-03-05 22:30:03 +0600

    Updated Valid Anagram solution

--- DSA-Must-Solve-75/1_Arrays_&_Strings/09_Valid_Anagram.js
+++ DSA-Must-Solve-75/1_Arrays_&_Strings/09_Valid_Anagram.js
@@ -2,3 +2,21 @@
 9. Valid Anagram
 Problem: Given two strings `s` and `t`, return true if `t` is an anagram of `s`, false otherwise.
 */
+function isAnagramBrute(s, t) {
+  if (s.length !== t.length) return false;
+  return s.split("").sort().join("") === t.split("").sort().join("");
+}
+
+// Optimal
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
+  }
+  return count.every((x) => x === 0);
+}
+
+console.log(isAnagramBrute("anagram", "nagaram"));
+console.log(isAnagram("anagram", "nagaram"));
