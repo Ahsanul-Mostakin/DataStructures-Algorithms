a49feda96a7c17a7643ef4d0dd66e8f41cf3d01e
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-03-08 21:42:39 +0600

    Updated Palindrome Check solution

--- DSA-Must-Solve-75/1_Arrays_&_Strings/14_Palindrome_Check.js
+++ DSA-Must-Solve-75/1_Arrays_&_Strings/14_Palindrome_Check.js
@@ -2,3 +2,22 @@
 14. Palindrome Check
 Problem: Given a string `s`, return true if it is a palindrome (ignoring non-alphanumeric characters and case).
 */
+function isPalindromeBrute(s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return s === s.split("").reverse().join("");
+}
+// Optimal
+function isPalindrome(s) {
+  s = s.toLowerCase();
+  let left = 0,
+    right = s.length - 1;
+  while (left < right) {
+    while (left < right && !/[a-z0-9]/.test(s[left])) left++;
+    while (left < right && !/[a-z0-9]/.test(s[right])) right--;
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
