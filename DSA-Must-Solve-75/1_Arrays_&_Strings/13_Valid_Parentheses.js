2749722f1986a1fa0bbf36c10bc11c29055bd598
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-03-08 21:42:39 +0600

    Updated Valid Parentheses solution

--- DSA-Must-Solve-75/1_Arrays_&_Strings/13_Valid_Parentheses.js
+++ DSA-Must-Solve-75/1_Arrays_&_Strings/13_Valid_Parentheses.js
@@ -2,3 +2,25 @@
 13. Valid Parentheses
 Problem: Given a string `s` containing only '(', ')', '{', '}', '[', ']', return true if the parentheses are valid (properly nested).
 */
+function isValidBrute(s) {
+  while (s.includes("()") || s.includes("[]") || s.includes("{}")) {
+    s = s.replace("()", "").replace("[]", "").replace("{}", "");
+  }
+  return s.length === 0;
+}
+// Optimal
+function isValid(s) {
+  const stack = [];
+  const brackets = { "(": ")", "{": "}", "[": "]" };
+  for (let char of s) {
+    if (brackets[char]) {
+      stack.push(char);
+    } else if (stack.length === 0 || brackets[stack.pop()] !== char) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(isValidBrute("()[]{}"));
+console.log(isValid("()[]{}"));
