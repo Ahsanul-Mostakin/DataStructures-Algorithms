d5b483e7450834f3c60bccb2456313d1ea5a6ba7
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-03-18 20:56:24 +0600

    04_Combination_Sum

--- DSA-Must-Solve-75/4_Recursion_&_Backtracking/04_Combination_Sum.js
+++ DSA-Must-Solve-75/4_Recursion_&_Backtracking/04_Combination_Sum.js
@@ -2,3 +2,42 @@
 4. Combination Sum 
 Problem: Given an array of distinct integers candidates and a target integer target, return all unique combinations of candidates that sum to target. Candidates can be used multiple times.
 */
+function combinationSumBrute(candidates, target) {
+  const result = [];
+  function generate(current, sum, start) {
+    if (sum === target) {
+      result.push([...current]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      current.push(candidates[i]);
+      generate(current, sum + candidates[i], i);
+      current.pop();
+    }
+  }
+  generate([], 0, 0);
+  return result;
+}
+// Optimal
+function combinationSum(candidates, target) {
+  const result = [];
+  function backtrack(current, sum, start) {
+    if (sum === target) {
+      result.push([...current]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      current.push(candidates[i]);
+      backtrack(current, sum + candidates[i], i);
+      current.pop();
+    }
+  }
+  backtrack([], 0, 0);
+  return result;
+}
+
+console.log(combinationSumBrute([2, 3, 6, 7], 7));
+console.log(combinationSum([2, 3, 6, 7], 7));
+// [[2, 2, 3], [7]]
